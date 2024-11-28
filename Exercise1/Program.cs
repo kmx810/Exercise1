dd7b7ef57da41beb82731366235a8c3856362492
kmx810 <kamil.d.maslanka@gmail.com>
2024-11-28 19:53:20 +0100

    CalculateAverage function added

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"Średnia: {average}");
+    }
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        double sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        return sum / numbers.Length;
+    }
+}
